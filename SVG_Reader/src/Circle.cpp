9766ea9be98768673ab6649f0fe782dbf7074d3e
Npeka <128128732+Npeka@users.noreply.github.com>
2023-11-12 09:28:03 +0700

    Revert "update"
    This reverts commit 2a6387596f1b1e5935430116d98e94e19406661e.

--- SVG_Reader/src/Circle.cpp
+++ SVG_Reader/src/Circle.cpp
@@ -2,36 +2,27 @@
 
 // Constructor
 Circle::Circle() {
-	cx = cy = r = 0;
+	
 }
 
 // Set attribute
 void Circle::setCX(const string& cx) {
-	this->cx = stof(cx);
+
 }
 
 void Circle::setCY(const string& cy) {
-	this->cy = stof(cy);
+
 }
 
 void Circle::setR(const string& r) {
-	this->r = stof(r);
+
 }
 
 // Virtual method
 void Circle::setAttribute(const string& attribute, const string& value) {
-	if (attribute == "cx") setCX(value);
-	else if (attribute == "cy") setCY(value);
-	else if (attribute == "r") setR(value);
+	
 }
 
 void Circle::draw(sf::RenderWindow& window, sf::Transform& transform) {
-	sf::CircleShape circle(r);
-	circle.setPosition(cx - r, cy - r);								// cx cy
-	if (isfill) circle.setFillColor(fill.sfColor());
-	else circle.setFillColor(sf::Color::Transparent);				// fill + fill-opacity
-	circle.setOutlineThickness(stroke_width);						// stroke-width
-	circle.setOutlineColor(stroke.sfColor());						// 
-	circle.setPointCount(2000);
-	window.draw(circle, transform);
+	
 }
