eba50f2e0dcc436c26a6a5a4e5668e70f713fdb4
LilMeen <nhkminh2004@gmail.com>
2023-11-24 10:30:28 +0700

    add new class: FPoint

--- SVG_Reader/src/Point.cpp
+++ SVG_Reader/src/Point.cpp
@@ -2,17 +2,28 @@
 
 Point::Point() {
 	x = y = 0;
-	fill = false;
 }
 
 Point::Point(const float& x, const float& y) {
 	this->x = x;
 	this->y = y;
-	fill = false;
 }
 
 Point::Point(const Point& point) {
 	x = point.x;
 	y = point.y;
-	fill = point.fill;
+}
+
+FPoint::FPoint() : Point() {
+	fill = false;
+}
+
+FPoint::FPoint(const float& x, const float& y) : Point(x, y) {
+	fill = false;
+}
+
+FPoint::FPoint(const FPoint& fpoint) {
+	x = fpoint.x;
+	y = fpoint.y;
+	fill = fpoint.fill;
 }
